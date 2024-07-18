3e99371c97e9e753c4eecdb0362a55bbc54f7001
Adarsh Raj <iamadarsh008@gmail.com>
2024-07-18 19:47:26 +0530

    Learning about the authentication login logout signup

--- README.md
+++
@@ -1,1 +0,0 @@
-# djanog-Learning
---
+++ geekyshows/enroll/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+
+
+# here it is for sign_up
+def sign_up(request):
+    if request.method == 'POST':
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request,'Account Created Successfully !!')
+            fm = SignUpForm()
+
+
+    else:
+        fm = SignUpForm()
+    return render(request,'enroll/signup.html',{'form':fm})
+
+
+# here it is for Login View
+def user_login(request):
+    if request.method == "POST":
+        fm = AuthenticationForm(request=request,data=request.POST)
+        if fm.is_valid():
+            uname = fm.cleaned_data['username']
+            upass = fm.cleaned_data['password']
+            user = authenticate(username=uname,password=upass)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect('/profile/')
+    else:
+        fm = AuthenticationForm()
+
+    return render(request, 'enroll/login.html',{'form':fm})
+
+def user_profile(request):
+    return render(request,'enroll/profile.html')
