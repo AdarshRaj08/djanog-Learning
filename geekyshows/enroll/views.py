6bc943ccd0c3a7159927ec9a74cee342afb0602a
Adarsh Raj <iamadarsh008@gmail.com>
2024-07-19 09:08:11 +0530

    password

--- geekyshows/enroll/views.py
+++ geekyshows/enroll/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,HttpResponseRedirect,HttpResponse
 from .forms import SignUpForm
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate, login, logout,update_session_auth_hash
 
 
 # Create your views here.
@@ -25,22 +25,74 @@ def sign_up(request):
 
 # here it is for Login View
 def user_login(request):
-    if request.method == "POST":
-        fm = AuthenticationForm(request=request,data=request.POST)
-        if fm.is_valid():
-            uname = fm.cleaned_data['username']
-            upass = fm.cleaned_data['password']
-            user = authenticate(username=uname,password=upass)
-            if user is not None:
-                login(request,user)
-                messages.success(request,'Logged in successfully !!')
-                return HttpResponseRedirect('/profile/')
+    # request.user.is_authenticated: it will check if the user is logged in or not 
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request,data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname,password=upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged in successfully !!')
+                    return HttpResponseRedirect('/profile/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'enroll/login.html',{'form':fm})
     else:
-        fm = AuthenticationForm()
-    return render(request, 'enroll/login.html',{'form':fm})
+        return HttpResponseRedirect('/profile/')
 
+
+# Profile
 def user_profile(request):
-    return render(request,'enroll/profile.html')
+    if request.user.is_authenticated:
+        return render(request,'enroll/profile.html',{'name':request.user})
+    else:
+        return HttpResponseRedirect('/login/')
 
-def home(request):
-    return HttpResponse('hii this is home')
+
+# Logout
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+
+# change password with old password
+def user_change_pass(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            fm = PasswordChangeForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                # when we change password then we logged out 
+                # update_session_auth_hash is essential when you want to keep the user logged in after they change their password. 
+                update_session_auth_hash(request,fm.user)
+                messages.success(request,'Password Changed Successfully')
+                return HttpResponseRedirect('/profile/')
+        else:
+            fm = PasswordChangeForm(user = request.user)
+        return render(request,'enroll/changepass.html',{'form':fm})
+
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+# change password without old password
+def user_change_pass1(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            fm = SetPasswordForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                # when we change password then we logged out 
+                # update_session_auth_hash is essential when you want to keep the user logged in after they change their password. 
+                update_session_auth_hash(request,fm.user)
+                messages.success(request,'Password Changed Successfully')
+                return HttpResponseRedirect('/profile/')
+        else:
+            fm = SetPasswordForm(user = request.user)
+        return render(request,'enroll/changepass1.html',{'form':fm})
+
+    else:
+        return HttpResponseRedirect('/login/')
--- geekyshows/geekyshows/urls.py
+++ geekyshows/geekyshows/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('',include('extra.urls')),
     path('login/',views.user_login,name='login'),
     path('signup/',views.sign_up,name='signup'),
-    path('home/',views.home,name='home'),
-    path('profile/',views.user_profile,name='profile')
+    path('profile/',views.user_profile,name='profile'),
+    path('logout/',views.user_logout,name='logout'),
+    path('changepass/',views.user_change_pass,name='changepass'),
+    path('changepass1/',views.user_change_pass1,name='changepass1'),
 
 ]
